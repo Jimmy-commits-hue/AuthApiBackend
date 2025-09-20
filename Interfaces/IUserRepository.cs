ed1b7c0c7b47468447c5e1ee4bbd5a8ad850377c
Jimmy <jabulanikhabana0@gmail.com>
2025-09-20 13:41:49 +0200

    Still yet to refractor some methods under UserService and test them

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -6,17 +6,17 @@ namespace AuthApi.Interfaces
     public interface IUserRepository
     {
 
-        Task CreateAsync(User user);
+        Task CreateAsync(User user, CancellationToken cancellationToken);
 
-        Task<User?> GetAsync(string customNumber);
+        Task<User?> GetAsync(string customNumber, CancellationToken cancellationToken);
 
-        Task<User?> GetUserByIdNumber(string idNumber);
+        Task<User?> GetUserByIdNumber(string idNumber, CancellationToken cancellationToken);
 
         Task<string> GetUserByCustomNumber(string customNumber);
 
         Task UpdateAsync(User user);
 
-        Task<User?> GetUserById(Guid userId);
+        Task<User?> GetUserById(Guid userId, CancellationToken cancellationToken);
 
         Task DeleteAsync(User user);
 
--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -7,21 +7,21 @@ namespace AuthApi.Interfaces
     public interface IUserService
     {
 
-        Task<Guid> CreateUserAsync(RegisterDto user);
+        Task<Guid> CreateUserAsync(RegisterDto user, CancellationToken cancellationToken);
 
-        Task<User> GetUserAsync(LoginDtos login);
+        Task<User> GetUserAsync(LoginDtos login, CancellationToken cancellationToken);
 
-        Task<User> GetUserById(Guid userId);
+        Task<User> GetUserById(Guid userId, CancellationToken cancellationToken);
 
         Task<string> GetUserNameByCustomNumber(string customNumber);
 
         Task UpdateUserNumberAsync(string customNumber, User user);
 
-        Task UpdateUserAsync(User user);
+        Task UpdateUserAsync(User user, CancellationToken cancellationToken);
 
         Task IsLoginEmailSent(User user);
 
-        Task DeleteUserAsync(DeleteUserDtos deleteUser);
+        Task DeleteUserAsync(DeleteUserDtos deleteUser, CancellationToken cancellationToken);
 
     }
 
